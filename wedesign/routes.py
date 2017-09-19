fef80d6803cf6555a593c8e95b8c7edea64d62c3
jianshentan <jianshentan@gmail.com>
2017-09-18 20:48:35 -0700

    updated

--- wedesign/routes.py
+++ wedesign/routes.py
@@ -8,7 +8,7 @@ bp = Blueprint('main', __name__)
 
 scrape_cmd = 'instagram-scraper --maximum 30 --tag'
 path_to_static_images = 'wedesign/static/images/'
-instagram_tag = 'lonelyselfie'
+instagram_tag = 'whoisunknown'
 
 
 def what_minute_is_it() -> int:
@@ -30,10 +30,7 @@ class Timer:
 
 timer = Timer(what_minute_is_it())
 
-
-@bp.route('/')
-def main():
-
+def scrapeInstagram():
     # run scraping script on load if another minute has passed
     last_time = timer.get_time()
     curr_time = what_minute_is_it()
@@ -42,11 +39,13 @@ def main():
         timer.set_time(curr_time)
         print('new minute {time}'.format(time=curr_time))
         
-        # scrape from instagram
+        # change directory to /static/images
         os.chdir( \
             os.path.join( \
                 os.path.abspath(sys.path[0]), 
                 path_to_static_images))
+
+        # scrape from instagram
         subprocess.call(scrape_cmd.split(' ') + [instagram_tag])
 
     # get list of names from pulled images
@@ -56,7 +55,6 @@ def main():
             path_to_static_images + instagram_tag))
 
     proc = subprocess.Popen('ls', stdout=subprocess.PIPE)
-
     instagram_images = proc \
         .stdout \
         .read() \
@@ -78,4 +76,43 @@ def main():
     random.shuffle(instagram_images_paths)
     instagram_images_paths = instagram_images_paths[0:20]
 
-    return render_template('index.html', instagram_images_url=instagram_images_paths)
+    return instagram_images_paths
+ 
+def getTileImages():
+    # change directory to /static/images/tiles
+    os.chdir( \
+        os.path.join( \
+            os.path.abspath(sys.path[0]), 
+            path_to_static_images + 'tiles'))
+
+    proc = subprocess.Popen('ls', stdout=subprocess.PIPE)
+    tile_images = proc \
+        .stdout \
+        .read() \
+        .decode('utf-8') \
+        .split('\n')
+
+    tile_images_paths = []
+    for i, val in enumerate(tile_images):
+        if len(val) == 0:
+            print("breaking out!")
+            break
+        tile_images_paths.append('tiles/' + val)
+
+    random.shuffle(tile_images_paths)
+    return tile_images_paths
+    
+
+@bp.route('/')
+def main():
+
+    # scape new images from instagram 
+    instagram_images_paths = scrapeInstagram()
+
+    # fetch all images from /images/tiles folder
+    tile_images_paths = getTileImages()
+   
+    return render_template('index.html', 
+            instagram_images_url=instagram_images_paths,
+            tile_images_url=tile_images_paths)
+
--- wedesign/static/js/index.js
+++ wedesign/static/js/index.js
@@ -1,14 +1,46 @@
-function setTileHeight() {
+function set_tile_height() {
     $(".tile").each(function() {
         width = $(this).width();
         $(this).height(width);
     });
 }
 
+/**
+ ** Shuffles array in place. ES6 version
+ ** @param {Array} a items The array containing the items.
+ **/
+function shuffle(a) {
+    for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
+}
+
+function load_images(urls) {
+    var tile_container = $("#tiles");
+    shuffle(urls);
+
+    // max_tiles, mt, is defined: mt = (n * 4) - 1
+    max_tiles = 27; 
+
+    for (i in urls) {
+        var url = urls[i];
+        tile = $("<div class='tile'><img src='/assets/images/" + url + "/'></div>");
+        tile_container.append(tile);
+        if (i >= max_tiles) {
+            break;
+        }
+    }
+}
+
 $(document).ready(function() {
-    setTileHeight();
+    load_images(imgs_url);
+    set_tile_height();
+
+    // need to run again for some reason...
+    setTimeout(set_tile_height, 100);
 });
 
 $(window).resize(function() {
-    setTileHeight();
+    set_tile_height();
 });
